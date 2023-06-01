bcdf0c757df1adc93d0d81ed0e8c5364e147f680
tanvir <himonislamfre@gmail.com>
2023-06-01 22:23:30 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express, { Application, Request, Response } from 'express'
-import cors from "cors"
-const app : Application = express()
+// import cors from "cors"
+const app: Application = express()
 
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }))
 
-
-app.get('/', (req : Request, res : Response) => {
-  res.send('Hello World!')
+app.get('/', (req: Request, res: Response) => {
+  res.send('working success')
 })
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,16 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log('Database connect')
-    } catch (error) {
-        console.log("fail to connect", error);
-    }
-    app.listen(config.port, () => {
-        console.log(`Example app listening on port ${config.port}`)
-    })
-
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database connect')
+  } catch (error) {
+    console.log('fail to connect', error)
+  }
+  app.listen(config.port, () => {
+    console.log(`Example app listening on port ${config.port}`)
+  })
 }
 main()
